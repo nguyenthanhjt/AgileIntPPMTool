19525592c586734bf2c93bf4c96a2be020342443
nguyenthanhjt <thanh.bn@outlook.com>
2021-11-08 00:14:29 +0700

    Fix bug relates to Updating project: remove sending payload:eerr.response.data when get error in dispatch

--- ppmtool-react-client/src/actions/projectActions.js
+++ ppmtool-react-client/src/actions/projectActions.js
@@ -14,7 +14,7 @@ export const createProject = (project, history) => async dispatch => {
   } catch (err) {
     dispatch({
       type: GET_ERRORS,
-      payload: err.response.data,
+      payload: {}
     });
   }
 };
