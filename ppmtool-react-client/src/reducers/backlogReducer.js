4d567bf5ab8a1856b801b8b9a09238f86afcb729
nguyenthanhjt <thanh.bn@outlook.com>
2021-11-24 00:27:06 +0700

    Create folder structure,components,type,reducers for ProjectBoard

---
+++ ppmtool-react-client/src/reducers/backlogReducer.js
@@ -0,0 +1,35 @@
+import {
+  GET_BACKLOG,
+  GET_PROJECT_TASK,
+  DELETE_PROJECT_TASK,
+} from "../actions/types";
+
+const initialState = {
+  projectTasks: [],
+  projectTask: {},
+};
+
+export default function (state = initialState, action) {
+  switch (action.type) {
+    case GET_BACKLOG:
+      return {
+        ...state,
+        projectTasks: action.payload,
+      };
+
+    case GET_PROJECT_TASK:
+      return {
+        ...state,
+        projectTask: action.payload,
+      };
+    case DELETE_PROJECT_TASK:
+      return {
+        ...state,
+        
+        // TODO: implement logic here
+      };
+
+    default:
+      return state;
+  }
+}
--- ppmtool-react-client/src/reducers/index.js
+++ ppmtool-react-client/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
+import backlogReducer from "./backlogReducer";
 import errorReducer from "./errorReducer";
 import projectReducer from "./projectReducer";
 
 export default combineReducers({
     errors: errorReducer,
-    project: projectReducer
+    project: projectReducer,
+    backlog: backlogReducer
 });
