4752faf68b9a0558391ab2743c4b3bb9b0c92dbb
nguyenthanhjt <thanh.bn@outlook.com>
2021-12-31 00:04:27 +0700

    BE: Add User entity,service,repository,validation

--- PMMToolFullStack/src/main/java/io/agileintelligence/ppmtool/domain/User.java
+++ PMMToolFullStack/src/main/java/io/agileintelligence/ppmtool/domain/User.java
@@ -1,4 +1,96 @@
 package io.agileintelligence.ppmtool.domain;
 
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import java.util.Date;
+
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Email(message = "Username needs to be an email")
+    @NotBlank(message = "Username is required")
+    @Column(unique = true)
+    private String username;
+
+    @NotBlank(message = "Please enter your full name")
+    private String fullName;
+
+    @NotBlank(message = "Password field is required")
+    private String password;
+
+    @Transient
+    private String confirmationPassword;
+
+    // One to many with Project
+
+    private Date createdAt;
+    private Date updatedAt;
+
+    protected void onCreate() {
+        this.createdAt = new Date();
+    }
+
+    protected void onUpdate() {
+        this.updatedAt = new Date();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getConfirmationPassword() {
+        return confirmationPassword;
+    }
+
+    public void setConfirmationPassword(String confirmationPassword) {
+        this.confirmationPassword = confirmationPassword;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
 }
